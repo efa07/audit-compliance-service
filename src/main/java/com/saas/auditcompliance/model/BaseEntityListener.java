b0a89e4ccd3c20ea71d7e24bd19e1b74367ab45e
efa <efatariku07@gmail.com>
2026-08-25 15:45:08 +0300

    modified:   src/main/java/com/saas/auditcompliance/model/Base.java 	modified:   src/main/java/com/saas/auditcompliance/model/BaseEntityListener.java 	modified:   src/main/java/com/saas/auditcompliance/utility/SpringContextHolder.java

--- src/main/java/com/saas/auditcompliance/model/BaseEntityListener.java
+++ src/main/java/com/saas/auditcompliance/model/BaseEntityListener.java
@@ -1,1 +1,18 @@
 package com.saas.auditcompliance.model;
+
+import com.saas.auditcompliance.utility.SecurityUtil;
+import com.saas.auditcompliance.utility.SpringContextHolder;
+import jakarta.persistence.PrePersist;
+
+import java.util.UUID;
+
+public class BaseEntityListener {
+
+    @PrePersist
+    public void setTenantId(Base entity) {
+        if (entity.getTenantId() == null) {
+            SecurityUtil securityUtil = SpringContextHolder.getBean(SecurityUtil.class);
+            entity.setTenantId(UUID.fromString(securityUtil.getTenantId()));
+        }
+    }
+}
