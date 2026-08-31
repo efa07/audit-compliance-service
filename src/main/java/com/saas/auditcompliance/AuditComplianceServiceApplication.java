db75a807eadcf3b19ea6a0c0f6447a946e5cae4a
efa <efatariku07@gmail.com>
2026-08-31 10:35:15 +0300

    modified:   src/main/java/com/saas/auditcompliance/AuditComplianceServiceApplication.java

--- src/main/java/com/saas/auditcompliance/AuditComplianceServiceApplication.java
+++ src/main/java/com/saas/auditcompliance/AuditComplianceServiceApplication.java
@@ -1,1 +1,17 @@
 package com.saas.auditcompliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
+public class AuditComplianceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditComplianceServiceApplication.class, args);
+    }
+}
+
